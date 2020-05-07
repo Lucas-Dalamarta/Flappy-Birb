c2f72b611225f864c78ce85777caeb2243266c75
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-06 23:39:50 -0300

    Added screen changing

--- game.js
+++ game.js
@@ -8,87 +8,146 @@ const context = canvas.getContext('2d');
 
 const flappyBirb = {
   sx: 0,
-  sy: 0,
-  sWidth: 33,
-  sHeight: 24,
-  dx: 10,
-  dy: 50,
+  sY: 0,
+  w: 33,
+  h: 24,
+  x: 10,
+  y: 50,
+  speed: 0,
+  gravity: 0.25,
+  update() {
+    flappyBirb.speed = flappyBirb.speed + flappyBirb.gravity;
+    flappyBirb.y = flappyBirb.y + flappyBirb.speed;
+  },
   draw() {
     context.drawImage(
       sprites,
-      flappyBirb.sx, flappyBirb.sy,
-      flappyBirb.sWidth, flappyBirb.sHeight,
-      flappyBirb.dx, flappyBirb.dy,
-      flappyBirb.sWidth, flappyBirb.sHeight,
+      flappyBirb.sx, flappyBirb.sY,
+      flappyBirb.w, flappyBirb.h,
+      flappyBirb.x, flappyBirb.y,
+      flappyBirb.w, flappyBirb.h,
     );
   },
 }
 
 const floor = {
   sx: 0,
-  sy: 610,
-  sWidth: 224,
-  sHeight: 112,
-  dx: 0,
-  dy: canvas.height - 112,
+  sY: 610,
+  w: 224,
+  h: 112,
+  x: 0,
+  y: canvas.height - 112,
   draw() {
     context.drawImage(
       sprites,
-      floor.sx, floor.sy,
-      floor.sWidth, floor.sHeight,
-      floor.dx, floor.dy,
-      floor.sWidth, floor.sHeight,
+      floor.sx, floor.sY,
+      floor.w, floor.h,
+      floor.x, floor.y,
+      floor.w, floor.h,
     );
 
     context.drawImage(
       sprites,
-      floor.sx, floor.sy,
-      floor.sWidth, floor.sHeight,
-      (floor.sx + floor.sWidth), floor.dy,
-      floor.sWidth, floor.sHeight,
+      floor.sx, floor.sY,
+      floor.w, floor.h,
+      (floor.sx + floor.w), floor.y,
+      floor.w, floor.h,
     );
   },
 }
 
 const background = {
-  sx: 390,
-  sy: 0,
-  sWidth: 275,
-  sHeight: 204,
-  dx: 0,
-  dy: canvas.height - 204,
+  sX: 390,
+  sY: 0,
+  w: 275,
+  h: 204,
+  x: 0,
+  y: canvas.height - 204,
   draw() {
-    context.fillStyle= '#70c5ce';
+    context.fillStyle = '#70c5ce';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.drawImage(
       sprites,
-      background.sx, background.sy,
-      background.sWidth, background.sHeight,
-      background.dx, background.dy,
-      background.sWidth, background.sHeight,
+      background.sX, background.sY,
+      background.w, background.h,
+      background.x, background.y,
+      background.w, background.h,
     );
 
     context.drawImage(
       sprites,
-      background.sx, background.sy,
-      background.sWidth, background.sHeight,
-      (background.dx + background.sWidth), background.dy,
-      background.sWidth, background.sHeight,
+      background.sX, background.sY,
+      background.w, background.h,
+      (background.x + background.w), background.y,
+      background.w, background.h,
     );
   },
 }
 
+const getReayImage = {
+  sX: 134,
+  sY: 0,
+  w: 174,
+  h: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  draw() {
+    context.drawImage(
+      sprites,
+      getReayImage.sX, getReayImage.sY,
+      getReayImage.w, getReayImage.h,
+      getReayImage.x, getReayImage.y,
+      getReayImage.w, getReayImage.h,
+    );
+  },
+}
 
+/**
+ * [Screens]
+ */
 
+let activeScreen = {};
+function changeToScreen(newScreen) {
+  activeScreen = newScreen;
+}
 
+const Screens = {
+  begin: {
+    draw() {
+      background.draw();
+      floor.draw();
+      flappyBirb.draw();
+      getReayImage.draw();
+    },
+    click() {
+      changeToScreen(Screens.game);
+    },
+    update() { }
+  },
+  game: {
+    draw() {
+      background.draw();
+      floor.draw();
+      flappyBirb.draw();
+    },
+    update() {
+      flappyBirb.update();
+    }
+  }
+}
 
 function loop() {
-  background.draw();
-  floor.draw();
-  flappyBirb.draw();
-  
+  activeScreen.update();
+  activeScreen.draw();
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function() {
+  if (activeScreen.click){
+    activeScreen.click();
+  }
+});
+
+changeToScreen(Screens.begin);
 loop();
